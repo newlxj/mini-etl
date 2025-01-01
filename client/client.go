d7135b2bc64342f13b1c923c1818982e7c07282b
lxj <inlhx@163.com>
2025-01-01 23:51:59 +0800

    增加日志增删改记录，以及根据主键更新数据

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -13,21 +14,58 @@ import (
 )
 
 type RowData struct {
-	Action    string        `json:"action"`
-	TableName string        `json:"table_name"`
-	Columns   []string      `json:"columns"`
-	Rows      []interface{} `json:"rows"`
+	Action     string        `json:"action"`
+	TableName  string        `json:"table_name"`
+	Columns    []string      `json:"columns"`
+	Rows       []interface{} `json:"rows"`
+	PrimaryKey []string      `json:"primary_key"`
+}
+
+type ClientConfig struct {
+	TaskName    string `json:"taskName"`
+	TaskAccount string `json:"taskAccount"`
+	TaskDB      string `json:"taskDB"`
+	TaskTable   string `json:"taskTable"`
+	Client      struct {
+		DBType     string `json:"dbType"`
+		DBUrl      string `json:"dbUrl"`
+		DBName     string `json:"dbName"`
+		DBUsername string `json:"dbUsername"`
+		DBPassword string `json:"dbPassword"`
+	} `json:"client"`
+}
+
+func loadClientConfig(filename string) ([]ClientConfig, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var configs []ClientConfig
+	if err := json.Unmarshal(data, &configs); err != nil {
+		return nil, err
+	}
+	return configs, nil
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/chat")
+	clientConfigs, err := loadClientConfig("clientConfig.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(clientConfigs) == 0 {
+		log.Fatal("No client configurations found")
+	}
+	clientConfig := clientConfigs[0]
+
+	db, err := sql.Open("mysql", clientConfig.Client.DBUsername+":"+clientConfig.Client.DBPassword+"@tcp("+clientConfig.Client.DBUrl+")/"+clientConfig.Client.DBName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	for {
-		resp, err := http.Get("http://127.0.0.1:8080/consume")
+		url := "http://127.0.0.1:8080/consume?taskAccount=" + clientConfig.TaskAccount + "&taskDB=" + clientConfig.TaskDB + "&taskTable=" + clientConfig.TaskTable
+		resp, err := http.Get(url)
 		if err != nil {
 			log.Error(err)
 			time.Sleep(2 * time.Second)
@@ -50,17 +88,61 @@ func main() {
 				continue
 			}
 
-			if rowData.Action == "insert" {
+			switch rowData.Action {
+			case "insert":
 				columns := strings.Join(rowData.Columns, ", ")
 				values := strings.Repeat("?, ", len(rowData.Columns))
 				values = values[:len(values)-2] // Remove the trailing comma and space
-				// query := "INSERT INTO " + rowData.TableName + " (" + columns + ") VALUES (" + values + ")"
 				query := "INSERT INTO " + rowData.TableName + "_001 (" + columns + ") VALUES (" + values + ")"
 				_, err = db.Exec(query, rowData.Rows...)
 				if err != nil {
 					log.Error(err)
 				} else {
 					log.Info("Data inserted successfully")
+				}
+			case "update":
+				setClause := ""
+				for _, col := range rowData.Columns {
+					setClause += col + " = ?, "
+				}
+				setClause = setClause[:len(setClause)-2] // Remove the trailing comma and space
+
+				whereClause := ""
+				for _, pk := range rowData.PrimaryKey {
+					whereClause += pk + " = ? AND "
+				}
+				whereClause = whereClause[:len(whereClause)-5] // Remove the trailing " AND "
+
+				query := "UPDATE " + rowData.TableName + "_001 SET " + setClause + " WHERE " + whereClause
+
+				// Split the rows into old and new values
+				numColumns := len(rowData.Columns)
+				oldValues := rowData.Rows[:numColumns]
+				newValues := rowData.Rows[numColumns:]
+
+				// Append the primary key values to the new values for the WHERE clause
+				primaryKeyValues := oldValues[:len(rowData.PrimaryKey)]
+				args := append(newValues, primaryKeyValues...)
+
+				_, err = db.Exec(query, args...)
+				if err != nil {
+					log.Error(err)
+				} else {
+					log.Info("Data updated successfully")
+				}
+			case "delete":
+				whereClause := ""
+				for _, pk := range rowData.PrimaryKey {
+					whereClause += pk + " = ? AND "
+				}
+				whereClause = whereClause[:len(whereClause)-5] // Remove the trailing " AND "
+
+				query := "DELETE FROM " + rowData.TableName + "_001 WHERE " + whereClause
+				_, err = db.Exec(query, rowData.Rows[:len(rowData.PrimaryKey)]...)
+				if err != nil {
+					log.Error(err)
+				} else {
+					log.Info("Data deleted successfully")
 				}
 			}
 		}
--- server/server.go
+++ server/server.go
@@ -20,15 +20,27 @@ type MyEventHandler struct {
 }
 
 type RowData struct {
-	Action    string        `json:"action"`
-	TableName string        `json:"table_name"`
-	Columns   []string      `json:"columns"`
-	Rows      []interface{} `json:"rows"`
+	Action     string        `json:"action"`
+	TableName  string        `json:"table_name"`
+	Columns    []string      `json:"columns"`
+	Rows       []interface{} `json:"rows"`
+	PrimaryKey []string      `json:"primary_key"`
 }
 
 type BinlogPosition struct {
 	Name string `json:"name"`
 	Pos  uint32 `json:"pos"`
+}
+
+type ServerConfig struct {
+	DBType     string   `json:"dbType"`
+	DBUrl      string   `json:"dbUrl"`
+	DBUsername string   `json:"dbUsername"`
+	DBPassword string   `json:"dbPassword"`
+	DBDBs      []string `json:"dbDBs"`
+	DataDelete bool     `json:"dataDelete"`
+	DataUpdate bool     `json:"dataUpdate"`
+	DataInsert bool     `json:"dataInsert"`
 }
 
 func flattenRows(rows [][]interface{}) []interface{} {
@@ -51,10 +63,11 @@ func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 	rowData := RowData{
-		Action:    e.Action,
-		TableName: e.Table.Name,
-		Columns:   getColumnNames(e.Table),
-		Rows:      flattenRows(e.Rows),
+		Action:     e.Action,
+		TableName:  e.Table.Name,
+		Columns:    getColumnNames(e.Table),
+		Rows:       flattenRows(e.Rows),
+		PrimaryKey: getPrimaryKeyColumns(e.Table),
 	}
 	data, err := json.Marshal(rowData)
 	if err != nil {
@@ -74,6 +87,16 @@ func getColumnNames(table *schema.Table) []string {
 		columnNames = append(columnNames, col.Name)
 	}
 	return columnNames
+}
+
+func getPrimaryKeyColumns(table *schema.Table) []string {
+	var primaryKeyColumns []string
+	for _, col := range table.Columns {
+		if table.IsPrimaryKey(table.FindColumn(col.Name)) {
+			primaryKeyColumns = append(primaryKeyColumns, col.Name)
+		}
+	}
+	return primaryKeyColumns
 }
 
 func (h *MyEventHandler) String() string {
@@ -134,14 +157,33 @@ func (h *MyEventHandler) OnPosSynced(header *replication.EventHeader, pos mysql.
 	return nil
 }
 
+func loadServerConfig(filename string) ([]ServerConfig, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var configs []ServerConfig
+	if err := json.Unmarshal(data, &configs); err != nil {
+		return nil, err
+	}
+	return configs, nil
+}
+
 func main() {
+	serverConfigs, err := loadServerConfig("serverConfig.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(serverConfigs) == 0 {
+		log.Fatal("No server configurations found")
+	}
 	cfg := canal.NewDefaultConfig()
-	cfg.Addr = "127.0.0.1:3306"
-	cfg.User = "root"
-	cfg.Password = "123456"
-	cfg.Dump.TableDB = "chat"
-	cfg.Dump.Tables = []string{"users"}
-	cfg.Dump.ExecutionPath = "" // Disable mysqldump
+	cfg.Addr = serverConfigs[0].DBUrl
+	cfg.User = serverConfigs[0].DBUsername
+	cfg.Password = serverConfigs[0].DBPassword
+	cfg.Dump.TableDB = serverConfigs[0].DBDBs[0]
+	cfg.Dump.Tables = []string{"users"} // This should be dynamically set based on the request
+	cfg.Dump.ExecutionPath = ""         // Disable mysqldump
 
 	filename := cfg.User + "-" + cfg.Dump.TableDB + "-" + cfg.Dump.Tables[0] + ".blog"
 	handler, err := NewMyEventHandler(filename)
@@ -153,6 +195,10 @@ func main() {
 	// Start the HTTP server in a separate goroutine
 	go func() {
 		http.HandleFunc("/consume", func(w http.ResponseWriter, r *http.Request) {
+			taskAccount := r.URL.Query().Get("taskAccount")
+			taskDB := r.URL.Query().Get("taskDB")
+			taskTable := r.URL.Query().Get("taskTable")
+			filename := taskAccount + "-" + taskDB + "-" + taskTable + ".blog"
 			data, err := handler.ReadAndClear(filename)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
